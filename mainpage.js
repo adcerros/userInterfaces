6f124ba1688be470e2efa92c69d30ab92780a03f
Adrian Cerros <adriancerrossanchez@gmail.com>
2021-10-19 06:12:50 +0200

    boton login y pop up creado

---
+++ mainpage.js
@@ -0,0 +1,12 @@
+// Boton de login
+$(document).ready(function(){
+    $("#logIn-btn").click(function(){
+        $("#logIn-popup").show(); 
+    });
+    $("#clslogIn-popup").click(function(){
+        $("#logIn-popup").hide(); 
+    });
+});
+
+
+clslogIn-popup
